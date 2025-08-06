19835483bcb00b9b157d1b7cb740443256f05026
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 <lindiwekhumalo833@gmail.com>
2025-08-06 21:20:29 +0200

    updated

--- Advanced_shell/apiAutomation-0x00/fetch_pikachu.sh
+++ Advanced_shell/apiAutomation-0x00/fetch_pikachu.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-curl -s https://pokeapi.co/api/v2/pokemon/pikachu -o pikachu.json
+# Fetch data from the Pokémon API for Pikachu
+response=$(curl -s -w "%{http_code}" -o data.json https://pokeapi.co/api/v2/pokemon/pikachu)
+
+# Check if request was successful
+if [ "$response" -ne 200 ]; then
+  echo "Error: Failed to fetch data for Pikachu. Status code: $response" >> errors.txt
+  exit 1
+fi
 
