988fc44402483729bbbee5adbf69b24b7b2c76f5
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 <lindiwekhumalo833@gmail.com>
2025-08-06 21:14:54 +0200

    Automate the process of making API requests to the Pokémon API and saving the results to a file

--- Advanced_shell/apiAutomation-0x00/fetch_pikachu.sh
+++ Advanced_shell/apiAutomation-0x00/fetch_pikachu.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-URL="https://pokeapi.co/api/v2/pokemon/pikachu"
-OUTPUT="data.json"
-ERROR_LOG="errors.txt"
-
-# Make the API request
-curl -s --fail "$URL" -o "$OUTPUT"
-if [ $? -ne 0 ]; then
-  echo "Failed to fetch Pikachu data on $(date)" >> "$ERROR_LOG"
-else
-  echo "Successfully fetched Pikachu data"
-fi
+curl -s https://pokeapi.co/api/v2/pokemon/pikachu -o pikachu.json
 
